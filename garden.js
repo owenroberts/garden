59923de6ac3daf1475a3d83e7ef3fafb05e54b04
Owen Roberts <obroberts@gmail.com>
2019-08-15 12:12:23 -0400

    added some basic stuff

---
+++ garden.js
@@ -0,0 +1,112 @@
+function loadSprites(file, json) {
+	for (const key in json) {
+		const params = { label: key, type: file, center: true, ...json[key] }
+		Game[file][key] = file == 'ui' ? new UI(params) : new Item(params);
+		for (let i = 0; i < json[key].scenes.length; i++) {
+			const scene = json[key].scenes[i];
+			if (!Game.scenes.includes(scene)) Game.scenes.push(scene);
+		}
+	}
+} /* data tied to edi, decouple later */
+
+Game.sprites = {};
+Game.ui = {};
+Game.scenes = [];
+Game.init({
+	canvas: "lines",
+	width: window.innerWidth,
+	height: window.innerHeight,
+	lps: 12,
+	mixedColors: true
+});
+Game.load({ ui: "/data/ui.json", sprites: "/data/sprites.json" }, loadSprites, Game.start);
+
+let player;
+
+function start() {
+	Game.scene = 'game';
+	player = new Player('drawings/cat.json', Game.width/2, Game.height/2);
+}
+
+function update() {
+
+	if (Game.scene == 'game') 
+		player.update();
+
+	const offset = {
+		x: Game.width - player.x,
+		y: Game.height - player.y
+	};
+
+	for (const key in Game.sprites) {
+		if (Game.sprites[key].scenes.includes(Game.scene))
+			Game.sprites[key].update(offset);
+	}
+}
+
+function draw() {
+
+	if (Game.scene == 'game')
+		player.display();
+
+	for (const key in Game.sprites) {
+		if (Game.sprites[key].scenes.includes(Game.scene))
+			Game.sprites[key].display();
+	}
+
+	for (const key in Game.ui) {
+		if (Game.ui[key].scenes.includes(Game.scene)) 
+			Game.ui[key].display();
+	}
+}
+
+/* events */
+function keyDown(key) {
+	switch (key) {
+		case 'a':
+		case 'left':
+			player.inputKey('left', true);
+			break;
+		case 'w':
+		case 'up':
+			player.inputKey('up', true);
+			break;
+		case 'd':
+		case 'right':
+			player.inputKey('right', true);
+			break;
+		case 's':
+		case 'down':
+			player.inputKey('down', true);
+			break;
+
+		case 'e':
+			// socket.emit('key interact', true);
+			break;
+	}
+}
+
+function keyUp(key) {
+	switch (key) {
+		case 'a':
+		case 'left':
+			player.inputKey('left', false);
+			break;
+		case 'w':
+		case 'up':
+			player.inputKey('up', false);
+			break;
+		case 'd':
+		case 'right':
+			player.inputKey('right', false);
+			break;
+		case 's':
+		case 'down':
+			player.inputKey('down', false);
+			break;
+
+		case 'e':
+			// socket.emit('key interact', false);
+			break;
+	}
+}
---
+++ player.js
@@ -0,0 +1,65 @@
+class Player extends Sprite {
+	constructor(src, x, y, debug) {
+		super(x, y);
+		this.x = x;
+		this.y = y;
+		this.center = true; /* need better name */
+		
+		// this.position.x += Game.width/2;
+		// this.position.y += Game.height/2;
+
+		this.debug = debug || false;
+		this.speed = new Cool.Vector(5, 5);
+		
+		this.addAnimation(src, () => {
+			this.animation.states = {
+				"idle": { "start": 0, "end": 4 },
+				"right": { "start": 4, "end": 8 },
+				"left": { "start": 8, "end": 12 },
+				"down": { "start": 12, "end": 14 },
+				"up": { "start": 14, "end": 16 },
+				"flower": { "start": 16, "end": 19 },
+				"skull": { "start": 19, "end": 23 },
+				"apple-x": { "start": 23, "end": 27 },
+				"heart": { "start": 27, "end": 32 },
+				"wave": { "start": 32, "end": 36 },
+				"drop-heart": { "start": 36, "end": 40 },
+				"drop-flower": { "start": 40, "end": 43 },
+				"drop-skull": { "start": 43, "end": 46 }
+			}; /* obvi needs to be in data */
+			this.animation.state = 'idle';
+		});
+
+		this.bounds = { top: -2048, bottom: 2048, left: -2048, right: 2048 }; /* where ???, Game? */
+		this.input = { right: false, up: false, left: false, down: false };
+	}
+
+	inputKey(key, state) {
+		this.input[key] = state;
+	}
+
+	update() {
+		let state = 'idle';
+		if (this.input.up) {
+			if (this.y > this.bounds.top)
+				this.y -= this.speed.y;
+			state = 'up';
+		}
+		if (this.input.down) {
+			if (this.y < this.bounds.bottom)
+				this.y += this.speed.y;
+			state = 'down';
+		}
+		if (this.input.right) {
+			if (this.x < this.bounds.right)
+				this.x += this.speed.x;
+			state = 'right';
+		}
+		if (this.input.left) {
+			if (this.x > this.bounds.left)
+				this.x -= this.speed.x;
+			state = 'left';
+		}
+		this.animation.setState(state);
+	}
+}
